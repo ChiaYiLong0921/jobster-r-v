92cf9ef7f97df63c2281351672504c16fd7dd746
Eric <e_chia0921@outlook.com>
2026-02-04 04:51:47 +0800

    set env

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -3,8 +3,7 @@ import { getUserFromLocalStorage, isDebug } from '.'
 import { clearStore } from '../features/user/userSlice'
 
 const local_api = '//localhost:8080/api/v1'
-const render_url = 'https://temp-jobster-api-dc9e.onrender.com/api/v1'
-const cloud_run = 'https://temp-jobster-api-1041608169052.europe-west1.run.app/api/v1'
+const cloud_run = import.meta.env.BACKEND_URL
 
 const customFetch = axios.create({
   // baseURL: local_api,
