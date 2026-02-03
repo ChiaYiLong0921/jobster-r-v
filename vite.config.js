65de71f636014481a22cd0b50f40cd90787cd03f
Eric <e_chia0921@outlook.com>
2026-02-04 05:11:27 +0800

    update port

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 8080,
+    port: process.env.PORT || 5173,
   },
   plugins: [react()],
 })
