1bdf732b2e00d9687ecc95231a61238682567e16
Eric <e_chia0921@outlook.com>
2026-02-19 14:09:35 +0800

    add vconsole for debug

--- src/components/StatItem.jsx
+++ src/components/StatItem.jsx
@@ -3,7 +3,7 @@ import Wrapper from '../assets/wrappers/StatItem'
 
 const StatItem = ({ count, title, icon, color, bcg }) => {
   return (
-    <Wrapper color={color} bcg={bcg}>
+    <Wrapper color={color} $bcg={bcg}>
       <header>
         <span className="count">{count}</span>
         <span className="icon">{icon}</span>
